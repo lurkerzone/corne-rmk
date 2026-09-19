a9050130a07ccd6c649011a69e547c06975c364a
lurkerzone <152053191+lurkerzone@users.noreply.github.com>
2026-09-19 16:08:33 +0800

    Update peripheral.rs

--- src/peripheral.rs
+++ src/peripheral.rs
@@ -1,7 +1,19 @@
 #![no_main]
 #![no_std]
 
+mod nice_view; // NEW
+
 use rmk::macros::rmk_peripheral;
 
 #[rmk_peripheral(id = 0)]
-mod keyboard_peripheral {}
+mod keyboard_peripheral {
+  /// NEW: right-half nice!view.
+    #[register_processor(event)]
+    fn nice_view() -> ::rmk::display::DisplayProcessor
+        crate::nice_view::NiceView,
+        ::rmk::display::OledRenderer,
+    > {
+        crate::nice_view::processor()
+    }
+}
+
